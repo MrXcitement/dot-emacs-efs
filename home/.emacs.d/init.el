a419acecf74836655728901fbf2b9d98b27d5592
Mike Barker <mike@thebarkers.com>
2024-01-13 13:07:57 -0700

    Initial commit

---
+++ home/.emacs.d/init.el
@@ -0,0 +1,10 @@
+;;; init.el --- A minimal emacs configuration
+
+;; Mike Barker <mike@thebarkers.com>
+;; January 13th, 2024
+
+;; This configuration was influnced from the "Emacs From Scratch" youtube series.
+;; https://www.youtube.com/watch?v=OaF-N-FuGtc&list=PLEoMzSkcN8oNmd98m_6FoaJseUsa6QGm2&pp=iAQB
+
+
+(setq inhibit-startup-message t)
