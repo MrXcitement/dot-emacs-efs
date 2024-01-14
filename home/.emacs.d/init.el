14b84193a6d99e38371648f0efc470de515c1c02
Mike Barker <mike@thebarkers.com>
2024-01-14 14:37:29 -0700

    develop: Style Your Emacs with the Modus Themes
    Load the builtin Modus Theme package and configure some basic settings
    Closes #3

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -13,9 +13,6 @@
 (scroll-bar-mode -1)
 (blink-cursor-mode -1)
 
-;; load a default theme, modus-theme included in emacs 28
-(load-theme 'modus-operandi)
-
 ;; save recent files
 (recentf-mode t)
 
@@ -38,3 +35,40 @@
 
 ;; revert non-file buffers, dired etc
 (setq global-auto-revert-non-file-buffers t)
+
+;;; Load modus theme, and customize it's settings
+
+;; Configure modus-theme: mode line
+(setq modus-themes-mode-line '(accented borderless (padding . 2) (height . 0.9)))
+
+;; Configure modus-theme: regions
+(setq modus-themes-region '(bg-only))
+
+;; Configure modus-theme: completion
+(setq modus-themes-completions
+          (quote ((matches . (extrabold background intense))
+                  (selection . (semibold accented intense))
+                  (popup . (accented)))))
+
+;; Configure modus-theme: syntax
+(setq modus-themes-bold-constructs t)
+(setq modus-themes-italic-constructs t)
+(setq modus-themes-paren-match '(bold intense underline))
+(setq modus-themes-syntax '(alt-syntax green-strings yellow-comments))
+
+;; Org Mode headings
+(setq modus-themes-headings
+      '((1 . (overline background  variable-pitch 1.3))
+        (2 . (overline rainbow 1.2))
+	(3 . (overline 1.1))
+        (t . (monochrome))))
+
+(setq modus-themes-scale-heading t)
+
+;; Org Mode Source blocks
+;; Note: if you change this, it will not be visible in currently opened org files until you restart org mode. `org-mode-restart`
+(setq modus-themes-org-blocks 'tinted-background)
+
+;; Load the modus theme
+(load-theme 'modus-operandi)
+
